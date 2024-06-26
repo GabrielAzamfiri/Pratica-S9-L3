f6e2279b3ac5baf6bc6bc9b7a92e1a00657fc239
GabrielAzamfiri <167236687+GabrielAzamfiri@users.noreply.github.com>
2024-06-26 15:25:42 +0200

    add ricerca che ti mostra i libri cercati

--- src/components/BookList.jsx
+++ src/components/BookList.jsx
@@ -1,5 +1,5 @@
 import { Component } from "react";
-import { Row } from "react-bootstrap";
+import { Form, InputGroup, Row } from "react-bootstrap";
 import SingleBook from "./SingleBook";
 
 
@@ -7,19 +7,37 @@ import SingleBook from "./SingleBook";
 
 class BookList extends Component  {
     state={
-       
+       ricerca: null,
     }
   
   render() {
+    
     return (
       <Row>
+        <InputGroup size="sm" className="mb-3">
+        <InputGroup.Text id="inputGroup-sizing-sm">Cerca Libro</InputGroup.Text>
+        <Form.Control
+          aria-label="Small"
+          aria-describedby="inputGroup-sizing-sm"
+          onChange={e => this.setState({ricerca :  e.target.value  })}
+        />
+      </InputGroup>
+      
         {/* faccio il map su state[nome array passato tramite props] */}
-        {this.props.category.map(book => {
+        {this.state.ricerca ? this.props.category.filter(libro=>libro.title.toLowerCase().includes(this.state.ricerca.toLowerCase())).map(book => {
             
-          return (
-            <SingleBook book={book}/>
-          );
-        })}
+            return (
+              
+              <SingleBook book={book}/>
+            )})
+            
+          
+           : this.props.category.map(book => {
+            
+            return (
+              <SingleBook book={book}/>
+            );
+          })}
       </Row>
     );
   }
